34635e06b8b4385aff4b18d30d3520b9a5ecb23f
SignpostMarv <me@signpostmarv.name>
2023-06-17 23:03:05 +0100

    amend schemas to be safer to validate & perform validation

---
+++ json-schema-validate.ts
@@ -0,0 +1,131 @@
+import Ajv, {JSONSchemaType} from 'ajv/dist/2019';
+
+import image_sources_schema from './data/image-sources.schema.json' assert {type: 'json'};
+import ytldp_schema from './data/yt-dlp.schema.json' assert {type: 'json'};
+import twitch_clips_schema from './data/twitch-clip-images.schema.json' assert {type: 'json'};
+import highlights_meta_schema from './data/highlights-meta.json' assert {type: 'json'};
+import grouped_meta_schema from './data/highlights-meta/grouped-meta.schema.json' assert {type: 'json'};
+
+import discord from './data/discord-images.json' assert {type: 'json'};
+import reddit from './data/reddit-images.json' assert {type: 'json'};
+import twitter from './data/twitter-images.json' assert {type: 'json'};
+import youtube from './data/youtube-images.json' assert {type: 'json'};
+import ytdlp from './data/has-author-but-no-links.json' assert {type: 'json'};
+import twitch_clips from './data/twitch-clip-images.json' assert {type: 'json'};
+import grouped_meta from './data/highlights-meta/grouped-meta.json' assert {type: 'json'};
+
+declare type image_source_type = {
+	[key: string]: {
+		src: string,
+		skip?: boolean,
+		alt?: string,
+		highlightsMeta?: string[],
+	}[],
+};
+
+declare type highlights_meta_short_type = {
+	timestamp: number, // currently required to also be present in timestamps_for_links for yt_dlp_type
+	meta: string[], // refer to highlights-meta.json
+};
+
+declare type ytldp_type = {
+	[key: string]: { // date of video
+		[key: string]: { // id of video
+			[key: string]: { // raw line
+				timestamp: number,
+				raw_text: string, // raw line without the youtube chapter time
+				timestamps_for_links: number[], // timestamps to pass to yt-dlp to grab an image. sorta redundant.
+				highlightsMeta: highlights_meta_short_type[],
+			}
+		},
+	},
+};
+
+declare type twitch_clips_type = { // key-value pair of twitch clip urls and an array of highlights meta values
+	[key: string]: highlights_meta_short_type[],
+};
+
+declare type grouped_meta_type = {
+	[key: string]: grouped_meta_type|string[],
+};
+
+const fudge_highlights_meta_schema = Object.assign(
+	{},
+	highlights_meta_schema,
+	{
+		$id: `data/${highlights_meta_schema.$id}`, // the schemas were designed for IDE use, so need to fudge this path
+	}
+);
+
+const ajv = new Ajv({
+	schemas: [
+		highlights_meta_schema,
+		fudge_highlights_meta_schema,
+		ytldp_schema,
+	],
+});
+
+async function validate<T>(json:any, schema:JSONSchemaType<T>) : Promise<T> {
+	return new Promise((yup, nope) => {
+		const validator = ajv.compile(schema);
+
+		if ( ! validator(json)) {
+			nope(validator.errors);
+		} else {
+			yup(json);
+		}
+	});
+}
+
+const validations_to_make: [
+	[JSONSchemaType<image_source_type>, {[key: string]: object}],
+	[JSONSchemaType<ytldp_type>, {[key: string]: object}],
+	[JSONSchemaType<twitch_clips_type>, {[key: string]: object}],
+	[JSONSchemaType<grouped_meta_type>, {[key: string]: object}],
+] = [
+	[
+		(image_sources_schema as unknown) as JSONSchemaType<image_source_type>,
+		{
+			discord,
+			reddit,
+			twitter,
+			youtube,
+		},
+	],
+	[
+		(ytldp_schema as unknown) as JSONSchemaType<ytldp_type>,
+		{
+			ytdlp,
+		},
+	],
+	[
+		(twitch_clips_schema as unknown) as JSONSchemaType<twitch_clips_type>,
+		{
+			twitch_clips,
+		},
+	],
+	[
+		(grouped_meta_schema as unknown) as JSONSchemaType<grouped_meta_type>,
+		{
+			grouped_meta,
+		},
+	],
+];
+
+for (const validation_set of validations_to_make) {
+	const [schema, image_sources] = validation_set;
+
+	for (const image_source of Object.entries(image_sources)) {
+		const [name, source] = image_source;
+
+		try {
+			await validate(source, schema);
+
+			console.log(`${name} is valid`);
+		} catch (err) {
+			console.error(err);
+
+			throw new Error(`${name} contains errors!`);
+		}
+	}
+}
